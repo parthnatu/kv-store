d57236f0cbe851b0effe9b8b032ce28c9a8b372e
chandan047 <chandanakiti@gmail.com>
2020-11-08 18:33:15 -0500

    commands

--- install_commands.sh
+++ install_commands.sh
@@ -2,26 +2,26 @@ export MY_INSTALL_DIR=$HOME/.local
 mkdir -p $MY_INSTALL_DIR
 export PATH="$PATH:$MY_INSTALL_DIR/bin"
 
+sudo apt-get update
 sudo apt install -y cmake
-
 sudo apt install -y build-essential autoconf libtool pkg-config
 
-sudo apt-get update
 sudo apt-get install git
-
 git clone --recurse-submodules -b v1.33.1 https://github.com/grpc/grpc
-cd grpc
 
+cd grpc
 mkdir -p cmake/build
 pushd cmake/build
 cmake -DgRPC_INSTALL=ON \
       -DgRPC_BUILD_TESTS=OFF \
       -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \
       ../..
-make 
+make -j4
 sudo make install
 popd
 
-export PKG_CONFIG_PATH=/lib/pkgconfig
-sudo apt-get install build-essential autoconf libtool pkg-config
-sudo apt-get install libssl-dev
+# export PKG_CONFIG_PATH=/lib/pkgconfig
+# sudo apt-get install build-essential autoconf libtool pkg-config
+# sudo apt-get install libssl-dev
+
+git clone https://github.com/parthnatu/kv-store.git
